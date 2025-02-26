7a93b3beaae233bbc44089972cadad33ab4ad5a3
Devan Pellow <devanpellow@gmail.com>
2025-02-26 18:55:15 +0100

    add page navigation arrows

--- app/components/mobile-preview/index.tsx
+++ app/components/mobile-preview/index.tsx
@@ -3,61 +3,79 @@
 import React, { useState } from 'react';
 import { Funnel } from '@/app/types/blocks';
 import DeviceRadioGroup from './DeviceRadioGroup';
-import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
-import { convertTitleToSlug } from '@/app/utils';
+import UrlBar from './UrlBar';
+import ChevronLeftIcon from '@heroicons/react/24/outline/ChevronLeftIcon';
+import ChevronRightIcon from '@heroicons/react/24/outline/ChevronRightIcon';
+import { getDeviceWidthStyles } from '@/app/utils';
+import Blocks from '../blocks/Blocks';
+
 function MobilePreview({ funnelData }: { funnelData: Funnel | null }) {
 	const [screenSize, setScreenSize] = useState<'small' | 'medium' | 'large'>(
 		'medium'
 	);
 
-	const getDeviceWidth = () => {
-		switch (screenSize) {
-			case 'small':
-				return 'w-[375px] h-[600px]';
-			case 'medium':
-				return 'w-[540] h-[600px]';
-			case 'large':
-				return 'w-[768px] h-[600px]';
-			default:
-				return 'w-[375px] h-[600px]';
-		}
-	};
+	const [pageIndex, setPageIndex] = useState(0);
+	const currentPage = funnelData?.pages[pageIndex];
+	const canGoBack = pageIndex > 0;
+	const canGoForward = funnelData && pageIndex < funnelData.pages.length - 1;
 
 	return (
-		<div>
+		<div className="w-full">
 			<DeviceRadioGroup screenSize={screenSize} setScreenSize={setScreenSize} />
 			<div className="flex flex-row items-center justify-center rounded-lg">
-				<div
-					className={`${getDeviceWidth()} transition-all duration-300 ease-in-out bg-white border border-gray-200 rounded-lg`}
-				>
-					{funnelData && (
-						<div
-							className="w-full h-full rounded-lg"
-							style={{ backgroundColor: funnelData.bgColor }}
-						>
-							<div className="flex flex-row items-center justify-between w-full h-8 bg-gray-400 px-2 rounded-t-lg">
-								<div className="flex flex-row gap-1 items-center">
-									<div className="w-3 h-3 bg-red-500 rounded-full"></div>
-									<div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-									<div className="w-3 h-3 bg-green-500 rounded-full"></div>
-								</div>
-								<div className="flex items-center justify-center w-3/4 h-3/4 bg-gray-200 rounded-full">
-									{`www.${convertTitleToSlug(funnelData.name)}.com`}
-								</div>
-								<div className="flex items-center justify-center hover:bg-gray-200 rounded-full p-1">
-									<PlusIcon className="w-4 h-4" />
+				<>
+					<div
+						className={`flex flex-row items-center justify-center w-10 h-10 rounded-full bg-gray-200 mr-4 shadow-sm ${
+							canGoBack
+								? 'opacity-100 cursor-pointer hover:bg-gray-300 hover:scale-110 transition-all duration-300 ease-in-out'
+								: 'opacity-50'
+						}`}
+						onClick={() => {
+							if (canGoBack) {
+								setPageIndex(pageIndex - 1);
+							}
+							return;
+						}}
+					>
+						<ChevronLeftIcon className="w-6 h-6" />
+					</div>
+
+					<div
+						className={`${getDeviceWidthStyles(
+							screenSize
+						)} transition-all duration-300 ease-in-out bg-white border border-gray-200 rounded-lg shadow-lg`}
+					>
+						{funnelData && (
+							<div className="flex flex-row w-full h-full">
+								<div
+									className="w-full h-full rounded-lg"
+									style={{ backgroundColor: funnelData.bgColor }}
+								>
+									<UrlBar title={funnelData.name} />
+									{currentPage?.blocks.map((block) => {
+										return <Blocks key={block.id} block={block} />;
+									})}
 								</div>
 							</div>
-							{funnelData.pages.map((page) => (
-								<div key={page.id}>
-									{page.blocks.map((block) => (
-										<div key={block.id}>{block.text}</div>
-									))}
-								</div>
-							))}
-						</div>
-					)}
-				</div>
+						)}
+					</div>
+
+					<div
+						className={`flex flex-row items-center justify-center w-10 h-10 rounded-full bg-gray-200 ml-4  shadow-sm ${
+							canGoForward
+								? 'opacity-100 cursor-pointer hover:bg-gray-300 hover:scale-110 transition-all duration-300 ease-in-out'
+								: 'opacity-50'
+						}`}
+						onClick={() => {
+							if (canGoForward) {
+								setPageIndex(pageIndex + 1);
+							}
+							return;
+						}}
+					>
+						<ChevronRightIcon className="w-6 h-6" />
+					</div>
+				</>
 			</div>
 		</div>
 	);
